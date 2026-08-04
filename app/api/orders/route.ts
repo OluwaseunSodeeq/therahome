ed14233bcff8349102917de4df557733b0a74676
OluwaseunSodeeq <ademolaoluwaseun90@gmail.com>
2026-08-04 03:52:01 +0100

    few features updated!!

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -186,6 +186,33 @@ export async function POST(req: Request) {
       },
     );
   }
+  /*
+import { sendTelegramMessage } from "@/lib/telegram";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await sendTelegramMessage(
+      "🎉 Telegram Bot is successfully connected to TheraHome!",
+    );
+    return NextResponse.json({
+      success: true,
+    });
+  } catch (error) {
+    console.error("Error sending Telegram message:", error);
+    return NextResponse.json(
+      {
+        success: false,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
+
+
+*/
 
   // } catch (error) {
   //   console.error("ORDER API ERROR:", error);
