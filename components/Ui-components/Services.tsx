c9451aa103cd036f9d280653f0eb60264bfb2fa5
OluwaseunSodeeq <ademolaoluwaseun90@gmail.com>
2026-08-09 21:32:04 +0100

    few features updated!!!

--- components/Ui-components/Services.tsx
+++ components/Ui-components/Services.tsx
@@ -56,8 +56,7 @@ export default function Services() {
           </ServiceReveal>
         </div>
       </div>
-      {/* <CallButton>Sucscribe to Our Vip Plans</CallButton> */}
-      <div className="w-full lg:max-w-56 mx-auto lg:mt-7">
+      <div className="mx-auto mt-5 w-fit lg:max-w-56 lg:mt-7 ">
         <PlanButton>Sucscribe to Our VIP Plans</PlanButton>
       </div>
     </section>
